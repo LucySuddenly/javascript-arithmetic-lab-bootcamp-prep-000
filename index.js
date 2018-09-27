2ab3e209252305a485a4750be4a21cc33b34f25a
Lucy Suddenly <suddenlylucy@gmail.com>
2018-09-27 23:08:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function inc(n) {
   n++
   return n
 }
+function dec(n) {
+  n--
+  return n
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
