63116e11e3237a213c2241751d3314ec8474dbc0
Lucy Suddenly <suddenlylucy@gmail.com>
2018-09-27 23:02:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function add(a,b) {
   return parseInt(a, 10) + parseInt(b, 10)
 }
+function subtract(a,b) {
+  return parseInt(a, 10) + parseInt(b, 10)
+}
+function multiply(a,b) {
+  return parseInt(a, 10) * parseInt(b, 10)
+}
+function divide(a,b) {
+  return parseInt(a, 10) / parseInt(b, 10)
+}
